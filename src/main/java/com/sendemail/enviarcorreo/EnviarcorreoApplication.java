863a43198288a337273beefb08bd49791fb43bb8
JDRH <jose.rivera@utp.edu.co>
2022-12-01 13:59:48 -0500

    first commit

---
+++ src/main/java/com/sendemail/enviarcorreo/EnviarcorreoApplication.java
@@ -0,0 +1,13 @@
+package com.sendemail.enviarcorreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviarcorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviarcorreoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sendemail/enviarcorreo/EnviarcorreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendemail.enviarcorreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviarcorreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
