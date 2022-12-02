2d42d60e0979bd619e598e91f93ee850e67e8a15
Jose <jose.rivera@utp.edu.co>
2022-12-02 17:18:47 -0500

    Envio de correo

--- src/main/java/com/sendemail/enviarcorreo/MailerApplication.java
+++ src/main/java/com/sendemail/enviarcorreo/MailerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SendMailApplication {
+public class MailerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SendMailApplication.class, args);
+		SpringApplication.run(MailerApplication.class, args);
 	}
 
 }
